729338054c3c9183d910e1683e58b47b6959ab45
Paula-Stefany <paula.sfa@outlook.com>
2025-08-15 17:04:33 -0300

    fix(details) tipagem dos projetos

---
+++ src/app/utils/types/project.ts
@@ -0,0 +1,10 @@
+export interface ProjectProps{
+  id: number; 
+  title: string;
+  technologies: string[];
+  description: string;
+  image_url: string;
+  project_url: string;
+  github_url: string;
+
+}
